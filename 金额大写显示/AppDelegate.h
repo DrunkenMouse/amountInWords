9b471268600c3235f4a8cd7bcf683d5694df9d1f
残月 <632342527@qq.com>
2016-09-13 09:21:24 +0800

    Initial Commit

---
+++ 金额大写显示/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  金额大写显示
+//
+//  Created by 王奥东 on 16/9/13.
+//  Copyright © 2016年 王奥东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 金额大写显示/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  金额大写显示
+//
+//  Created by 王奥东 on 16/9/13.
+//  Copyright © 2016年 王奥东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
